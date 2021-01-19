325b5f72642d2fbb3fd40bb94b623fdd840770d5
Enes Öztepe <enesoztepebjk@gmail.com>
2021-01-19 17:17:26 +0300

    CustomerManager classı tamamlandı

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer custom)
+        {
+            Console.WriteLine(custom.Name + " " + custom.Surname + " adlı müşteri eklenmiştir.");
+        }
+        public void Delete(Customer custom)
+        {
+            Console.WriteLine(custom.Name + " " + custom.Surname + " adlı müşteri silinmiştir.");
+        }
+        public void List(Customer[] custom)
+        {
+            Console.WriteLine("Müşteriler,\n");
+            foreach (var cus in custom)
+            {
+                Console.WriteLine("(" + cus.Id + ") " + cus.Name + " " + cus.Surname);
+            }
+        }
+    }
+}
